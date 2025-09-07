48b68f2cbd7490f1d6f652f2dfef8db3457511e7
ronak-pal1 <ronakpaul882@gmail.com>
2025-09-07 20:50:51 +0530

    Add [redis client, resolve url func]

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,9 +1,40 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/ronak-pal1/url-shortener-go/database"
+)
 
 
 func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+
+	if !govalidator.IsURL(url) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Please provide a valid url to resolve"})
+	}
 	
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "URL is successfully resolved"})
+	r := database.CreateClient(0)
+
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+
+
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found"})
+	}else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"Can't connect to the database"})
+	}
+
+	rInr := database.CreateClient(1);
+
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx,"counter")
+	
+	return c.Redirect(value, 301);	
+
 }
