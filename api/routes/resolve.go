35b9aacf4a3cf422eaac8ec0aed9489997413b37
ronak-pal1 <ronakpaul882@gmail.com>
2025-09-07 20:26:09 +0530

    Add [helpers, routes, main file]

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,9 @@
 package routes
+
+import "github.com/gofiber/fiber/v2"
+
+
+func ResolveURL(c *fiber.Ctx) error {
+	
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "URL is successfully resolved"})
+}
